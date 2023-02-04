cd6a03c052188d3060b092140aeb48799a8ef26b
Anthony <antharithm707@gmail.com>
2023-02-04 17:03:30 -0500

    stable with raw frontend

--- block-data-stable/src/App.js
+++ block-data-stable/src/App.js
@@ -1,22 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import { type } from '@testing-library/user-event/dist/type';
+
+// After you compile & deploy Greeter.sol - paste the contract address below
+// If Error = ABI is missing.. re-compile, re-deploy and paste that contract address below
+// npx hardhat run --network localhost scripts/deploy.js
+const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
+
+  const [greeting, setGreetingValue] = useState()
+
+  async function requestAccount() {
+
+    // Requests the account information from their metamask wallet
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function fetchGreeting() {
+
+    // If the user has MetaMask, 'window.ethereum' will detect if they are connected
+    if (typeof window.ethereum !== 'undefined') {
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+
+      } catch (error) {
+        console.log("Error: ", error)
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return // Checks if they actaully typed in a greeting on the frontend
+    if (typeof window.ethereum !== 'undefined') {
+
+      // Waits for the user to "Connect" to your site..
+      await requestAccount();
+
+      // Since we are creating an update to the blockchain, this allows them to create a tx through a 'signer'
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      // New instance of that contract 
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+
+      // This is whatever the user types into the input box as the greeting
+      const transaction = await contract.setGreeting(greeting)
+      setGreetingValue('')
+
+      // Waits for the tx to be confirmed onto the blockchain
+      await transaction.wait()
+
+      // Log the new greeting value from the blockchain
+      fetchGreeting()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='App Header'>
+        <button onClick={fetchGreeting}>Fetch Greeting</button>
+        <button onClick={setGreeting}>Set Greeting</button>
+        <input 
+          onChange={error => setGreetingValue(error.target.value)}
+          placeholder="Set greeting"
+          value={greeting}
+        />
       </header>
     </div>
   );
