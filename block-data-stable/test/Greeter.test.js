7662cb2bc03ecf3dd0d8896797c2490609f5cca6
Anthony Tilotta <antharithm707@gmail.com>
2023-02-06 19:13:27 -0500

    Update Greeter.test.js

--- block-data-stable/test/Greeter.test.js
+++ block-data-stable/test/Greeter.test.js
@@ -1,11 +1,4 @@
-const {
-  time,
-  loadFixture,
-} = require("@nomicfoundation/hardhat-network-helpers");
-const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe("Lock", function () {}
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
+describe("Greeter", function () {}
